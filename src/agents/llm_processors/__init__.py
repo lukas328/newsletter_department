d565358bd1a7ad1c7715fba8bed7dedfe9e552a3
lukas328 <73937925+lukas328@users.noreply.github.com>
2025-06-05 15:59:49 +0200

    Add ArticleWriterAgent using OpenAI web search

---
+++ src/agents/llm_processors/__init__.py
@@ -0,0 +1,9 @@
+from .summarizer_agent import SummarizerAgent
+from .categorizer_agent import CategorizerAgent
+from .article_writer_agent import ArticleWriterAgent
+
+__all__ = [
+    "SummarizerAgent",
+    "CategorizerAgent",
+    "ArticleWriterAgent",
+]
